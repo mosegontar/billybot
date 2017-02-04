014412add059c780b699ef015981a5ea40a5a8f9
Alex Gontar <mosegontar@gmail.com>
2017-02-04 13:41:15 -0500

    Add TestBillParser class: able to sanitize bill_id

--- billy.py
+++ billy.py
@@ -6,38 +6,72 @@ class SunlightAPI(object):
 
     def __init__(self):
         self.domain = 'https://congress.api.sunlightfoundation.com/'
-        self.congress = self.get_results(self.get_current_congress())[0]['congress']
+        self.congress = str(self.get_results(self.get_current_congress())[0]['congress'])
 
     def get_results(self, data):
         return json.loads(data)['results']
 
     def get_current_congress(self):
-        resp = requests.get(self.domain + 'bills')
+        url = self.domain + 'bills'
+        resp = requests.get(url)
         return resp.text
 
-    def get_legislators(self, last_name, party='', chamber=''):
+    def search_legislators(self, last_name, party='', chamber=''):
         url = self.domain + 'legislators?query={}&party={}&chamber={}'.format(last_name, 
                                                                               party, 
                                                                               chamber)
         resp = requests.get(url)
         return self.get_results(resp.text)
 
-    def get_bio_ids(self, data):
-        return [member['bioguide_id'] for member in data]    
+    def get_roll_call_vote(self, roll_id, bioguide_id):
+        url = self.domain + 'votes?roll_id={}&fields=voters.{}.vote'.format(roll_id,
+                                                                            bioguide_id)
+        resp = requests.get(url)
+        return self.get_results(resp.text)
 
-class LegislatorAPI(SunlightAPI):
+
+
+class LegislatorParser(SunlightAPI):
 
     def __init__(self, bioguide_id):
+
         super().__init__()
         self.bioguide_id = bioguide_id
 
-    def get_roll_call_vote(self, roll_id):
-        url = self.domain + 'votes?roll_id={}&fields=voters.{}.vote'.format(roll_id,
-                                                                             self.bioguide_id)
-        resp = requests.get(url)
-        data = self.get_results(resp.text)
-        vote = data[0]['voters'][self.bioguide_id]['vote']
+    @staticmethod
+    def get_bio_ids(data):
+        return [member['bioguide_id'] for member in data]
+
+    def parse_roll_call_vote(self, roll_id):
+        
+        data = self.get_roll_call_vote(roll_id, self.bioguide_id)
+        
+        try:
+            vote = data[0]['voters'][self.bioguide_id]['vote']
+        except IndexError:
+            return None
+
         return vote
+
+class BillParser(SunlightAPI):
+
+    def __init__(self, bill_id):
+        super().__init__()
+        self.bill_id = bill_id
+        self.sanitize_bill_id()
+
+    def sanitize_bill_id(self):
+
+        if not self.bill_id.endswith('-'+self.congress):
+            self.bill_id += '-' + self.congress
+
+        self.bill_id = self.bill_id.lower().replace('.', '')
+
+
+
+
+
+
 
 
 
--- test_billy.py
+++ test_billy.py
@@ -1,6 +1,7 @@
 import unittest
 
-from billy import SunlightAPI, LegislatorAPI
+from billy import SunlightAPI, LegislatorParser, BillParser
+
 
 class TestSunlightAPI(unittest.TestCase):
 
@@ -8,31 +9,49 @@ class TestSunlightAPI(unittest.TestCase):
         self.api = SunlightAPI()
 
     def test_can_get_most_recent_congress(self):
-        self.assertEqual(self.api.congress, 115)           
+        self.assertEqual(self.api.congress, '115')           
 
     def test_able_to_query_legislators_by_name(self):
-        data = self.api.get_legislators('Warren')
+        data = self.api.search_legislators('Warren')
         matches = [result['last_name'] == 'Warren' for result in data]
         self.assertTrue(any(matches))
 
     def test_able_to_narrow_resp_data_with_more_params(self):
-        data = self.api.get_legislators(last_name='Smith')
+        data = self.api.search_legislators(last_name='Smith')
         matches = [result['first_name'] == 'Jason' for result in data]
         self.assertTrue(any(matches))
-        data = self.api.get_legislators(last_name='Smith', party='D')
+        data = self.api.search_legislators(last_name='Smith', party='D')
         matches = [result['first_name'] == 'Jason' for result in data]
         self.assertFalse(any(matches))    
 
     def test_can_get_legislator_bio_id(self):
-        data = self.api.get_legislators(last_name='Warren')
-        self.assertIn('W000817', self.api.get_bio_ids(data))
+        data = self.api.search_legislators(last_name='Warren')
+        self.assertIn('W000817', LegislatorParser.get_bio_ids(data))
 
 
-class TestLegislatorAPI(unittest.TestCase):
+class TestLegislatorParser(unittest.TestCase):
 
     def setUp(self):
-        self.api = LegislatorAPI('W000817')
+        self.api = LegislatorParser('W000817')
 
     def test_can_get_particular_roll_call_vote(self):
-        vote = self.api.get_roll_call_vote('s26-2017')
+        vote = self.api.parse_roll_call_vote('s26-2017')
         self.assertEqual(vote, 'Nay')
+
+    def test_return_None_if_roll_call_vote_not_found(self):
+        vote = self.api.parse_roll_call_vote('x26-2017')
+        self.assertEqual(vote, None)
+
+class TestBillParser(unittest.TestCase):
+
+    def setUp(self):
+        self.api = BillParser('S.Con.Res.3')
+
+    def test_sanitize_bill_id_if_congress_left_out(self):
+        self.assertEqual(self.api.bill_id, 'sconres3-115')
+
+    def test_sanitize_bill_id_if_congress_included(self):
+        self.api.bill_id = 'S.Con.Res.3-115'
+        self.api.sanitize_bill_id()
+        self.assertEqual(self.api.bill_id, 'sconres3-115')
+
