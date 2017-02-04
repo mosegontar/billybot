4fa1f3e427b68c0518e9777f9167f76e0391721c
Alex Gontar <mosegontar@gmail.com>
2017-02-03 22:49:24 -0500

    Add SunlightParser class and LegislatorParser child class; Test that members retieved by bio id and retrieving most recent congress

--- billy.py
+++ billy.py
@@ -1,12 +1,32 @@
+import json
 import requests
 
+class SunlightParser(object):
+
+    def __init__(self, json_data):
+        self.data = json.loads(json_data)['results']
+
+    def get_most_recent_congress(self):
+        return self.data[0]['congress']
+
+class LegislatorParser(SunlightParser):
+
+    def get_bio_ids(self):
+        return [member['bioguide_id'] for member in self.data]
 
 class SunlightAPI(object):
     """https://sunlightlabs.github.io/congress/"""
 
     def __init__(self):
         self.domain = 'https://congress.api.sunlightfoundation.com/'
+        self.congress = self.get_most_recent_congress()
 
-    def make_request(self):
-        resp = requests.get(self.domain)
+    def get_most_recent_congress(self):
+        resp = requests.get(self.domain + 'bills')
         return resp
+
+    def get_legislators(self, last_name, party='', chamber=''):
+        url = self.domain + 'legislators?query={}&party={}&chamber={}'.format(last_name, party, chamber)
+        resp = requests.get(url)
+        return resp  
+
--- test_billy.py
+++ test_billy.py
@@ -1,10 +1,39 @@
 import unittest
 
-from billy import SunlightAPI
+from billy import SunlightAPI, SunlightParser, LegislatorParser
 
-class TestSunlightAPI(unittest.TestCase):
+class BaseTestCase(unittest.TestCase):
 
-    def test_can_200_resp_from_sunlight_domain(self):
-        api = SunlightAPI()
-        resp = api.make_request()
-        self.assertEqual(resp.status_code, 200)   
+    def setUp(self):
+        self.api = SunlightAPI()
+
+class TestSunlightAPI(BaseTestCase): 
+
+    def test_able_to_query_legislators_by_name(self):
+        resp = self.api.get_legislators('Warren')
+        self.assertIn('Warren', resp.text)
+
+    def test_able_to_narrow_resp_data_with_more_params(self):
+        resp = self.api.get_legislators(last_name='Smith')
+        self.assertIn('Jason', resp.text)        
+        resp = self.api.get_legislators(last_name='Smith', party='D')
+        self.assertNotIn('Jason', resp.text)
+
+    def test_can_get_most_recent_congress(self):
+        resp = self.api.get_most_recent_congress()
+        parser = SunlightParser(resp.text)
+        congress = parser.get_most_recent_congress()
+        self.assertEqual(congress, 115)
+
+
+class TestLegislatorParser(BaseTestCase):
+
+    def test_can_get_legislator_bio_id(self):
+        resp = self.api.get_legislators(last_name='Warren')
+        parser = LegislatorParser(resp.text)
+        self.assertIn('W000817', parser.get_bio_ids())
+
+#class TestBillParser(BaseTestCase):
+
+    
+
