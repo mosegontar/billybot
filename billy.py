a9c88f8da7c2245f74d7dc5e8a3e6e2e8369f1eb
Alex Gontar <mosegontar@gmail.com>
2017-02-04 12:36:51 -0500

    Refactor bill.py: LegislatorAPI directly inherits from SunlightAPI, SunlightParser removed; pass test for get_roll_call_vote method

--- billy.py
+++ billy.py
@@ -6,29 +6,40 @@ class SunlightAPI(object):
 
     def __init__(self):
         self.domain = 'https://congress.api.sunlightfoundation.com/'
-        self.data = None
-
-    def get_current_congress(self):
-        resp = requests.get(self.domain + 'bills')
-        return resp.text 
-
-class SunlightParser(SunlightAPI):
-
-    def __init__(self):
-        super().__init__()
-        self.data = None
         self.congress = self.get_results(self.get_current_congress())[0]['congress']
 
     def get_results(self, data):
         return json.loads(data)['results']
 
-class LegislatorAPI(SunlightParser):
+    def get_current_congress(self):
+        resp = requests.get(self.domain + 'bills')
+        return resp.text
 
     def get_legislators(self, last_name, party='', chamber=''):
-        url = self.domain + 'legislators?query={}&party={}&chamber={}'.format(last_name, party, chamber)
+        url = self.domain + 'legislators?query={}&party={}&chamber={}'.format(last_name, 
+                                                                              party, 
+                                                                              chamber)
         resp = requests.get(url)
-        self.data = self.get_results(resp.text)
-        return resp     
+        return self.get_results(resp.text)
 
-    def get_bio_ids(self):
-        return [member['bioguide_id'] for member in self.data]
+    def get_bio_ids(self, data):
+        return [member['bioguide_id'] for member in data]    
+
+class LegislatorAPI(SunlightAPI):
+
+    def __init__(self, bioguide_id):
+        super().__init__()
+        self.bioguide_id = bioguide_id
+
+    def get_roll_call_vote(self, roll_id):
+        url = self.domain + 'votes?roll_id={}&fields=voters.{}.vote'.format(roll_id,
+                                                                             self.bioguide_id)
+        resp = requests.get(url)
+        data = self.get_results(resp.text)
+        vote = data[0]['voters'][self.bioguide_id]['vote']
+        return vote
+
+
+
+
+
--- test_billy.py
+++ test_billy.py
@@ -1,32 +1,38 @@
 import unittest
 
-from billy import LegislatorAPI
+from billy import SunlightAPI, LegislatorAPI
 
-class TestLegislatorAPI(unittest.TestCase):
+class TestSunlightAPI(unittest.TestCase):
 
     def setUp(self):
-        self.api = LegislatorAPI()
+        self.api = SunlightAPI()
 
     def test_can_get_most_recent_congress(self):
         self.assertEqual(self.api.congress, 115)           
 
     def test_able_to_query_legislators_by_name(self):
-        resp = self.api.get_legislators('Warren')
-        self.assertIn('Warren', resp.text)
+        data = self.api.get_legislators('Warren')
+        matches = [result['last_name'] == 'Warren' for result in data]
+        self.assertTrue(any(matches))
 
     def test_able_to_narrow_resp_data_with_more_params(self):
-        resp = self.api.get_legislators(last_name='Smith')
-        self.assertIn('Jason', resp.text)        
-        resp = self.api.get_legislators(last_name='Smith', party='D')
-        self.assertNotIn('Jason', resp.text)    
+        data = self.api.get_legislators(last_name='Smith')
+        matches = [result['first_name'] == 'Jason' for result in data]
+        self.assertTrue(any(matches))
+        data = self.api.get_legislators(last_name='Smith', party='D')
+        matches = [result['first_name'] == 'Jason' for result in data]
+        self.assertFalse(any(matches))    
 
     def test_can_get_legislator_bio_id(self):
-        self.api.get_legislators(last_name='Warren')
-        self.assertIn('W000817', self.api.get_bio_ids())
+        data = self.api.get_legislators(last_name='Warren')
+        self.assertIn('W000817', self.api.get_bio_ids(data))
 
-     
 
-#class TestBillParser(BaseTestCase):
+class TestLegislatorAPI(unittest.TestCase):
 
-    
+    def setUp(self):
+        self.api = LegislatorAPI('W000817')
 
+    def test_can_get_particular_roll_call_vote(self):
+        vote = self.api.get_roll_call_vote('s26-2017')
+        self.assertEqual(vote, 'Nay')
