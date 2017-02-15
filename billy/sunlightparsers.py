3f055c42d5b2c7e08e17fd2d97c994683c2b1336
mosegontar <mosegontar@gmail.com>
2017-02-15 11:29:59 -0500

    Remove Bill related classes

--- billy/sunlightparsers.py
+++ billy/sunlightparsers.py
@@ -1,34 +1,47 @@
 from billy.sunlightapi import SunlightAPI
 
 
-class Parser(SunlightAPI):
+class MemberParser(object):
 
-    MEMBERS_OF_CONGRESS = SunlightAPI.get_all_members_of_congress()
+    MEMBERS_OF_CONGRESS = SunlightAPI.get_all_members_of_congress()   
+
+    @classmethod
+    def find_members(cls, query, zipcode=None):
+        """Return dictionary dict of legislator matches"""
+
+        initial_data = None
+        if zipcode:
+            initial_data = cls.find_member_by_zip(zipcode)
+            
+        data = cls.lookup_members(query)
+
+        if data:
+            found_members = [cls.summarize_bio(bio) for bio in data]
+        else:
+            return None
+
+        return found_members
+
+    @classmethod
+    def find_member_by_zip(cls, zipcode):
+        data = SunlightAPI.search_legislators_by_zip(zipcode)
+        return data        
 
     @classmethod
     def lookup_members(cls, keys, items=None):
 
         if not items:
             items = cls.MEMBERS_OF_CONGRESS
+
         key_words = keys.split()
+
         matches = []
+        
         for member in items:
             if all([k in member.values() for k in key_words]):
                 matches.append(member)
+        
         return matches
-
-class MemberParser(Parser):
-
-    def __init__(self, bioguide_id):
-        super().__init__()
-        self.bioguide_id = bioguide_id
-        self.member_data = self.get_member_data(self.bioguide_id)
-        self._recent_votes = None
-
-    @classmethod
-    def find_member_by_zip(cls, zipcode):
-        data = SunlightAPI.search_legislators_by_zip(zipcode)
-        return data
 
     @classmethod
     def formalize_name(cls, member_bio):
@@ -49,41 +62,4 @@ class MemberParser(Parser):
 
         return (member_summary, bio) #bio['bioguide_id'])
 
-    @classmethod
-    def find_members(cls, query, zipcode=None):
-        """Return dictionary dict of legislator matches"""
 
-        if zipcode:
-            initial_data = cls.find_member_by_zip(zipcode)
-            data = cls.lookup_members(query, initial_data)
-        
-        if not zipcode:
-            data = cls.lookup_members(query)
-
-        if data:
-            found_members = [cls.summarize_bio(bio) for bio in data]
-        else:
-            return None
-
-        return found_members
-
-    @property
-    def recent_votes(self):
-        """Return legislator's recent votes. If set to None, make api call."""
-
-        if not self._recent_votes:
-            self._recent_votes = self.get_legislator_recent_votes(self.bioguide_id)
-
-        return self._recent_votes
-
-    def parse_roll_call_vote(self, roll_id):
-        """Return legislator's roll call vote (Yea or Nay)."""
-
-        data = self.get_roll_call_vote(roll_id, self.bioguide_id)
-
-        try:
-            vote = data[0]['voters'][self.bioguide_id]['vote']
-        except:
-            return None
-
-        return vote
