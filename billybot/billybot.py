7ec4a26ea09693e67a75dc44b79f397f8a34c129
Alex Gontar <mosegontar@gmail.com>
2017-04-01 15:45:10 -0400

    Make threads daemons so that they die on MainThread exit; also ensure that we aren't accumulating too many old threads

--- billybot/billybot.py
+++ billybot/billybot.py
@@ -1,10 +1,12 @@
+import sys
 import time
-import random
 import threading
+import datetime
 
-from .config import BOT_ID, AT_BOT, READ_WEBSOCKET_DELAY, SLACK_CLIENT
 from .message_handler import ContactQueryMessageHandler
 from .query_handler import MemberQuery
+from .config import (BOT_ID, AT_BOT, READ_WEBSOCKET_DELAY, SLACK_CLIENT,
+    MAX_THREAD_AGE, OLD_THREAD_LIMIT)
 
 
 class BillyBot(object):
@@ -12,19 +14,19 @@ class BillyBot(object):
     def connect(self):
         """Connect to Slack RTM and start accepting incoming messages."""
 
-        MessageOperator.ACTIVE_QUERIES = dict()
-
         if SLACK_CLIENT.rtm_connect():
             print('BillyBot is running!')
 
             while True:
 
+                self._monitor_old_threads()
+
                 stream = SLACK_CLIENT.rtm_read()
                 user_id, username, command, channel = self.parse_stream(stream)
 
                 if command and channel:
-                    thread = MessageOperator(user_id, username, command, channel)
-                    thread.daemon = True
+                    thread = MessageTriage(user_id, username, command, channel)
+                    thread.daemon = True # daemons will die if MainThread exits
                     thread.start()
 
                 time.sleep(READ_WEBSOCKET_DELAY)
@@ -32,10 +34,12 @@ class BillyBot(object):
             print('Connection failed :(')
 
     def parse_stream(self, stream_output):
-        """
-            The Slack Real Time Messaging API is an events firehose.
-            this parsing function returns None unless a message is
-            directed at the Bot, based on its ID.
+        """The Slack Real Time Messaging API is an events firehose.
+
+        This parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+
+        https://www.fullstackpython.com/blog/build-first-slack-bot-python.html
         """
 
         output_list = stream_output
@@ -67,33 +71,66 @@ class BillyBot(object):
                         text = text.split(AT_BOT)[1].strip()
 
                     if DIRECT_MESSAGE or AT_BOT_REPLY:
+
                         return user_id, username, text, channel
 
         return None, None, None, None
 
+    def _monitor_old_threads(self):
+        """Check how many active and old threads are alive."""
 
-class MessageOperator(threading.Thread):
+        current_time = datetime.datetime.now().timestamp()
+
+        active_threads = [thread.time_alive(current_time)
+                          for thread in threading.enumerate()[1:]]
+
+        old_threads = [item >= MAX_THREAD_AGE for item in active_threads]
+
+        # If we have too many old threads, it means that Thread instances
+        # 'run' methods are taking a long time to return (if they are to
+        # return at all). This is an indication that there is some problem
+        # in our code and we exit.
+
+        if len(old_threads) > OLD_THREAD_LIMIT:
+
+            # print data on old threads to aid debugging
+            print('Old Threads: ')
+            for thread in old_threads:
+                print('{}: {} via {}'.format(thread.username,
+                                             thread.message,
+                                             thread.active_query))
+            sys.exit()
+
+
+class MessageTriage(threading.Thread):
 
     ACTIVE_QUERIES = dict()
 
     def __init__(self, user_id, username, command, channel):
+
         threading.Thread.__init__(self)
+        self._thread_initiated = datetime.datetime.now().timestamp()
+
+        self.name = user_id
         self.user_id = user_id
         self.username = username
         self.channel = channel
         self.message = command
-        self.active_query = MessageOperator.ACTIVE_QUERIES.get(self.user_id)
+        self.active_query = MessageTriage.ACTIVE_QUERIES.get(self.user_id)
 
     @classmethod
     def update_queries(cls, user_id=None, handler=None):
+        """Update active_queries dict with latest handler."""
 
         cls.ACTIVE_QUERIES[user_id] = handler
 
+    def time_alive(self, current_time):
+        """Return number of seconds the thread instance has been alive."""
+
+        return current_time - self._thread_initiated
+
     def run(self):
         """Triage message and prepare reply to send."""
-
-        if self.message == 'Warren':
-            time.sleep(30)
 
         query_handler, reply = self.process_query()
 
--- billybot/config.py
+++ billybot/config.py
@@ -12,17 +12,20 @@ AT_BOT = '<@{}>'.format(BOT_ID)
 SLACK_CLIENT = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 READ_WEBSOCKET_DELAY = 1
 
+# What counts an an 'old' thread
+MAX_THREAD_AGE = 90  # seconds
+OLD_THREAD_LIMIT = 10
 
 slack_attachment = {
-    "fallback": None, # "Required plain-text summary of the attachment."
-    "color": '#56A0D3', # color hex value
-    "pretext": None, #"Optional text that appears above the attachment block"
-    "author_name": None ,
+    "fallback": None,  # "Required plain-text summary of the attachment."
+    "color": '#56A0D3',  # color hex value
+    "pretext": None,  # "Optional text that appears above the attachment block"
+    "author_name": None,
     "author_link": None,
     "title": None,
     "title_link": None,
     "fields": [],
-    "text": None, #"Optional text that appears within the attachment",
+    "text": None,  # "Optional text that appears within the attachment",
     "footer": None,
     "ts": None
 }
